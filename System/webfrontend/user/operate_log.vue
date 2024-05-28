748bfd07784cd7626da0363d0488732b15f69087
zhouquan2k <zhouquan2k@gmail.com>
2024-05-27 23:14:06 -0400

    .enhanced: exception detail

--- System/webfrontend/user/operate_log.vue
+++ System/webfrontend/user/operate_log.vue
@@ -1,17 +1,24 @@
 <template>
     <div>
-        <crud ref="crud" name="OperateLog" desc="操作日志" :apis="apis" :actions="[]" :searchVisible="true">
-        </crud>
+        <!--crud ref="crud" name="OperateLog" desc="操作日志" :apis="apis" :actions="[]" :searchVisible="true">
+        </crud-->
+        <SimpleTable ref="table" :searchVisible="true" :columns="getEntityFields('OperateLog', 'listable')"
+            idCol="logId" :searches="getEntityFields('OperateLog', 'searchable')" :searchMethod="onSearch">
+            <template #expand="scope">
+                {{ scope.data.resultMsg }}
+            </template>
+        </SimpleTable>
         <el-button v-if="hasPermission('***')" type="text" @click="apis.testException()">test</el-button>
     </div>
 </template>
 <script>
 import operateLogApi from "@user/operate_log_api.js";
 import Crud from '@/components/Crud';
+import SimpleTable from '@/components/SimpleTable.vue';
 import { hasPermission } from '@/utils/utils';
 export default {
     components: {
-        Crud,
+        Crud, SimpleTable
     },
     data() {
         return {
@@ -20,6 +27,9 @@ export default {
     },
     methods: {
         hasPermission,
+        onSearch(params) {
+            return operateLogApi.search(params);
+        }
     }
 }
 </script>
